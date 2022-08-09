4d1d56267b577f7f688a91882f4600936679730c
Eduardo Franz <eduardo.franz77@gmail.com>
2022-08-09 01:57:27 -0300

    add componentes tema postagem-post,put,delete

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@ import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastropostagem/CadastroPost';
+import CadastroTema from './components/temas/cadastrotema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
 
 
 
@@ -18,8 +22,8 @@ function App() {
     <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
-        <Routes> 
-          
+        <Routes>
+
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
@@ -27,6 +31,18 @@ function App() {
 
           <Route path="/temas" element={<ListaTema />} />
           <Route path="/postagens" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
         </Routes>
       </div>
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -29,21 +29,21 @@ export const busca = async(url:any,setDado:any,header:any) => {
     setDado(resposta.data)
 }
 
-// export const buscaId = async(url: string, setDados: any,  header: any) => {
-//     const resposta = await api.get(url, header)
-//     setDados(resposta.data)
-// }
+export const buscaId = async(url: string, setDados: any,  header: any) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
 
-// export const post = async(url: string, dados: any, setDados: any,  header: any) => {
-//     const resposta = await api.post(url, dados, header)
-//     setDados(resposta.data)
-// }
+export const post = async(url: string, dados: any, setDados: any,  header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
 
-// export const put = async(url: string, dados: any, setDados: any,  header: any) => {
-//     const resposta = await api.put(url, dados, header)
-//     setDados(resposta.data)
-// }
+export const put = async(url: string, dados: any, setDados: any,  header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
 
-// export const deleteId = async(url: string, header: any) => {
-//     await api.delete(url, header)
-// }
+export const deleteId = async(url: string, header: any) => {
+    await api.delete(url, header)
+}
